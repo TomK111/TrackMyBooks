ac6e96d7842704b65ecd151947ea7416ee1c2cfb
Tomislav Kraljic <tomislavkraljic@outlook.com>
2020-12-19 18:59:56 -0800

    added crud functionality

--- Pages/BookList/Edit.cshtml.cs
+++ Pages/BookList/Edit.cshtml.cs
@@ -4,13 +4,40 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using TrackMyBooks.Model;
 
 namespace TrackMyBooks.Pages.BookList
 {
     public class EditModel : PageModel
     {
-        public void OnGet()
+        private ApplicationDbContext _db;
+
+        public EditModel(ApplicationDbContext db)
         {
+            _db = db;
+        }
+        [BindProperty]
+        public Book Book { get; set; }
+
+        public async Task OnGet(int id)
+        {
+            Book = await _db.Book.FindAsync(id);
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if(ModelState.IsValid)
+            {
+                var BookFromDb = await _db.Book.FindAsync(Book.Id);
+                BookFromDb.Name = Book.Name;
+                BookFromDb.Author = Book.Author;
+                BookFromDb.ISBN = Book.ISBN;
+
+                await _db.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+
+            return RedirectToPage();
         }
     }
 }
